c2de55ec095f18e5795d49aaf8082ae267a26762
12882357 <12882357@adsroot.uts.edu.au>
2018-10-16 12:28:36 +1100

    optimize the file read_copy_file.py

--- tools/read_copy_file.py
+++ tools/read_copy_file.py
@@ -2,7 +2,7 @@ import os
 
 file_w=open('1.sh','w')
 listFileName='/data/xiaozhou/Downloads/datasets/imagenet/caffe_ilsvrc12/val.txt'
-cpLabel = {'340':'zebra','603':'horse_cart','254':'pug'}
+cpLabel = {'242':'random500_1','309':'random500_2','310':'random500_3','347':'random500_4','348':'random500_5','349':'random500_6','357':'random500_7','358':'random500_8','359':'random500_9'}
 path2='/data/xiaozhou/Downloads/datasets/imagenet/imagenet12-val/'
 path3='/data/xiaozhou/Downloads/datasets/imagenet/testimages/'
 
@@ -14,15 +14,13 @@ for filename in listfiles:
     filename = filename.strip('\n')
     filename = filename.split(' ')
     print(filename)
-    if filename[-1] == '340':
-        file_w.writelines("cp -as "+path2+filename[0]+" "+path3+cpLabel['340']+'/')
-        file_w.writelines('\n')
-    elif filename[-1] == '603':
-        file_w.writelines("cp -as "+path2+filename[0]+" "+path3+cpLabel['603']+'/')
-        file_w.writelines('\n')
-    elif filename[-1] == '254':
-        file_w.writelines("cp -as "+path2+filename[0]+" "+path3+cpLabel['254']+'/')
-        file_w.writelines('\n')
+    for key,value in cpLabel.items():
+        if filename[-1] == key:
+           desDir = path3+value+'/'
+           if not os.path.exists(desDir):
+               os.mkdir(desDir)
+           file_w.writelines("cp "+path2+filename[0]+" "+path3+value+'/')
+           file_w.writelines('\n')
 
 file_w.close()
 val = os.system("sh 1.sh")
