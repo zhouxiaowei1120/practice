6e45c9cc90e343a52f92522d9edfe987788e0b37
DaveZhou <xiaowei.zhou@student.uts.edu.au>
2018-12-17 11:51:14 +1100

    modify plot file to plot different figures

--- tools/plot_loss.py
+++ tools/plot_loss.py
@@ -12,13 +12,14 @@ Output: loss_log.png The file of loss curve.
 import matplotlib.pyplot as plt
 import sys
 
-filename = input("Input:")
+#filename = input("Input:")
 #filename = "E:\Desktop\Cycle_GAN\CycleGAN-and-pix2pix-master\Experiments\horse2zebra\result\loss_log.txt"
-#filename = "/home/uts/param.log"
+filename = "../../param.log"
 show_freq = 1  # control whether print every loss on the figure. show_freq for print every 'show_freq'
 
 loss = []
 itera = []
+grad = []
 flag = False
 i = -1
 
@@ -30,19 +31,25 @@ else:
     filelines = f.readlines()
     for fileline in filelines:
         # print (fileline[25:30])
-        if fileline[25:30] == 'epoch':
+        if 'Generator Gradient:' in fileline:
             i += 1  
             if i % show_freq == 0:
-                fileline = fileline[25:-1]
-                fileline = fileline.split(',')
-                for eachline in fileline:
-                    eachline = eachline.replace(' ','').split(':')
-                    if flag:
-                        loss.append(float(eachline[1]))
-                        flag = False
-                    else:
-                        itera.append(int(eachline[1])) 
-                        flag = True
+                fileline = fileline.split(':')
+                grad.append(float(fileline[-1].strip()))
+                itera.append(i)
+        # if fileline[25:30] == 'epoch':
+        #     i += 1  
+        #     if i % show_freq == 0:
+        #         fileline = fileline[25:-1]
+        #         fileline = fileline.split(',')
+        #         for eachline in fileline:
+        #             eachline = eachline.replace(' ','').split(':')
+        #             if flag:
+        #                 loss.append(float(eachline[1]))
+        #                 flag = False
+        #             else:
+        #                 itera.append(int(eachline[1])) 
+        #                 flag = True
     f.close()
         
     
@@ -60,12 +67,15 @@ else:
     # plt.plot(itera,G_A,'g')
     # plt.plot(itera,Cyc_A,'b')
     # label = ["D_A","G_A","Cyc_A"]
-    plt.plot(itera,loss,'g')
-    label = ['training loss']
+    plt.plot(itera,grad,'g')
+    label = ['gradients']
     plt.legend(label,loc=0,ncol=3)
     
     title = 'Generate data on the boundary: dataset=\'imagenet\', iteration=500'
     plt.title(title)
     plt.savefig("loss_log.png")
+    plt.show()
+
+    plt.hist(grad,bins=100,normed=True)
     plt.show()
     
