cc5e7323368bdb91a8d6885728bd94ed81818f96
zxw <zhouxiaowei1120@gmail.com>
2022-05-01 15:26:26 +1000

    Update reszie_image.py
    chase the save type

--- tools/reszie_image.py
+++ tools/reszie_image.py
@@ -11,14 +11,16 @@ from PIL import Image
 
 
 #pathname = argv[1]
-pathname = "E:/Desktop/小脸儿/数据/测试数据/"
+pathname = "./"
 width = 800
-for root,dirs,filenames in os.walk("E:/Desktop/1/"):
+for root,dirs,filenames in os.walk(pathname):
     for imname in filenames:
+        if not 'png' in imname and not 'jpg' in imname:
+            continue
         im_tmp = Image.open(os.path.join(root,imname))
         (x,y) = im_tmp.size
-        print x,y
+        print (x,y)
         y = y * width // x
         im_resize = im_tmp.resize((width,y),Image.ANTIALIAS)
-        new_name = root +'new_'+imname 
+        new_name = os.path.join(root, 'new_'+imname)
         im_resize.save(new_name)
